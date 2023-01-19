8f8a61c416b9227ef5de08b7f839c6e67b54a00f
arongilberts <122982693+arongilberts1801@users.noreply.github.com>
2023-01-19 12:36:33 +0530

    contactapi

---
+++ index.js
@@ -0,0 +1,38 @@
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const textarea = document.getElementById("area");
+const phone =document.getElementById("phone");
+const submit= document.getElementsByClassName("form-contact")[0];
+
+submit.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    console.log("clicked");
+    
+
+    // write html for proper format to be send in mail
+    let ebody=`
+    <b>Name: </b>${fname.value}&nbsp;${lname.value}
+    <br>
+    <b>Email: </b>${email.value}
+    <br>
+    <b>Phone: </b>${phone.value}
+    <br>
+    <b>Subject: </b>${subject.value}
+    <br>
+    <b>TextArea:- </b>${textarea.value}
+ 
+`
+    // email code here
+    Email.send({
+         SecureToken : "072cd0ac-4872-460f-96b5-b316bd0d52ec ",
+        To : 'arongilberts1801@gmail.com',
+        From : "arongilberts1801@gmail.com",
+        Subject : "Testing email from" + email.value,
+        Body : ebody
+    }).then(
+      message => alert(message)  
+    ); 
+
+});
